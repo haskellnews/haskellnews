14a7643c72c4dfc3109c34d57284d1cc494f2c3c
Chris Done <chrisdone@gmail.com>
2013-03-17 17:37:51 +0100

    Add title.

--- src/HN/View/Template.hs
+++ src/HN/View/Template.hs
@@ -4,11 +4,13 @@
 module HN.View.Template where
 
 import HN.View
+import qualified Text.Blaze.Html5 as H (title)
 
 template name innerhead innerbody = do
   docType
   html $ do
-    head $ do link ! rel "stylesheet" ! type_ "text/css" ! href "/css/bootstrap.min.css"
+    head $ do H.title "Haskell News"
+              link ! rel "stylesheet" ! type_ "text/css" ! href "/css/bootstrap.min.css"
               link ! rel "stylesheet" ! type_ "text/css" ! href "/css/bootstrap-responsive.css"
               link ! rel "stylesheet" ! type_ "text/css" ! href "/css/haskellnews.css"
               meta ! httpEquiv "Content-Type" ! content "text/html; charset=UTF-8"
