7b0405d6832b2e08b2b575ae903af28d15df6c3f
Chris Done <chrisdone@gmail.com>
2013-03-17 15:12:56 +0100

    Make some tables.

--- src/HN/Model/Migrations.hs
+++ src/HN/Model/Migrations.hs
@@ -10,4 +10,18 @@ import Snap.App
 -- | Migrations.
 versions :: [(Int,Model c s Int64)]
 versions = zip [1..] ms where
-  ms = []
+  ms = [ex ["CREATE TABLE source"
+           ,"(id serial primary key"
+           ,",title text not null"
+           ,");"
+           ,""
+           ,"CREATE TABLE item "
+           ,"(id serial primary key"
+           ,",source integer not null references source(id) on delete cascade on update cascade"
+           ,",added timestamp with time zone not null default now()"
+           ,",published timestamp with time zone not null"
+           ,",title text not null"
+           ,",description text not null"
+           ,");"]
+       ]
+  ex q = exec q ()
