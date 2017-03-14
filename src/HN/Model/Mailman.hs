1c512b8ae0c81f582bde6ba07983c2572e28b67b
Luke Murphy <lukewm@riseup.net>
2017-03-14 16:45:43 +0000

    Fix errors revealed from `--pedantic` build.
    This also closes #54 since the Travis build is back.

--- src/HN/Model/Mailman.hs
+++ src/HN/Model/Mailman.hs
@@ -4,32 +4,26 @@
 -- and https://github.com/haskell-infra/hl/issues/63#issuecomment-74420850
 
 {-# language OverloadedStrings #-}
-{-# language BangPatterns #-}
 
 module HN.Model.Mailman where
 
 import Network.HTTP.Conduit
--- I tried with
--- import HN.Curl
--- but could not get it to work reliably:
--- segfaults, badcertfile, and other erratic behaviour
--- on opening the second HTTPS connection.
 
 import Text.Feed.Types
 import Text.Feed.Constructor
 
 import qualified Text.HTML.DOM
 import Text.XML
-import Text.XML.Cursor
-import qualified Data.Text as T 
+import Text.XML.Cursor as XMLCursor
+import Text.XML.Cursor.Generic as XMLGeneric
+import qualified Data.Text as T
 
 import Data.Time.Format
 import Data.Time.LocalTime ( ZonedTime )
--- import Data.Time ( rfc822DateFormat, defaultTimeLocale )
 import Data.Char (isDigit)
 
-test1 = do
-  downloadFeed 10 "https://mail.haskell.org/pipermail/haskell-cafe/"
+test1 :: IO (Either String Feed)
+test1 = downloadFeed 10 "https://mail.haskell.org/pipermail/haskell-cafe/"
 
 
 -- | look at archive and produce a feed.
@@ -54,11 +48,11 @@ downloadFeed its uri= do
             [] -> "Tue Feb 17 18:55:05 UTC 2015"
             d : _ -> d
       let feed0 = newFeed (RSSKind Nothing)
-          feed = id
-               $ withFeedPubDate pubdate
-               $ foldr addItem feed0 items
+          feed = withFeedPubDate pubdate
+              $ foldr addItem feed0 items
       return $ Right feed
 
+getDoc :: String -> IO (Either a Document)
 getDoc uri = do
   result <- simpleHttp uri
   return $ Right $ Text.HTML.DOM.parseLBS result
@@ -74,7 +68,7 @@ getItems its uri dates =
     d:ates -> do
       result <- getDoc $ uri ++ T.unpack d
       let items = case result of
-            Left err -> []
+            Left _ -> []
             Right doc ->
               let cursor = fromDocument doc
               in  descendant cursor
@@ -82,31 +76,35 @@ getItems its uri dates =
                   >>= hasAttribute "HREF"
                   >>= parent
                   >>= element "LI"
-                  >>= mkItem uri (dropSuffix "thread.html" d ) 
+                  >>= mkItem uri (dropSuffix "thread.html" d )
       later <- getItems (its - length items) uri ates
       return $ items ++ later
 
+dropSuffix :: T.Text -> T.Text -> T.Text
 dropSuffix suf s =
   if T.isSuffixOf suf s
   then T.take (T.length s - T.length suf) s
   else s
 
+fetchDate :: XMLGeneric.Cursor Node -> T.Text -> [String]
 fetchDate cursor desc = do
   d <- fetch cursor desc
   -- we get this from mailman "Tue Feb 17 18:55:05 UTC 2015"
   -- and apparently we need "Tue, 17 Feb 2015 21:12:55 UTC"
-  t <- maybe [] return $ parseTime defaultTimeLocale "%a %b %d %H:%M:%S %Z %Y" $ T.unpack d
+  t <- maybe [] return $ parseTimeM True defaultTimeLocale "%a %b %d %H:%M:%S %Z %Y" $ T.unpack d
   return $ formatTime defaultTimeLocale rfc822DateFormat ( t :: ZonedTime )
 
+fetch :: XMLGeneric.Cursor Node -> T.Text -> [T.Text]
 fetch cursor desc = descendant cursor
                     >>= element "b"
                     >>= hasChildContents desc
                     >>= followingSibling
                     >>= element "i" >>= child >>= content
 
-hasChildContents desc =
-  check $ \ c -> desc == T.concat (child c >>= content )
-        
+hasChildContents :: T.Text -> XMLCursor.Axis
+hasChildContents desc = check $ \ c -> desc == T.concat (child c >>= content )
+
+mkItem :: String -> T.Text -> XMLGeneric.Cursor Node -> [Item]
 mkItem uri d c = do
   href <- child c >>= element "A" >>= laxAttribute "HREF"
   title <- child c >>= element "A" >>= hasAttribute "HREF"
@@ -118,9 +116,9 @@ mkItem uri d c = do
   -- and we need to get to this part: --------------------^^^^^^^^^^^
   -- which gives the epoch time of the current message
   NodeComment com <- map node ( precedingSibling c >>= precedingSibling )
-  let (s,t) = T.breakOnEnd "." com
+  let (s,_) = T.breakOnEnd "." com
       n = T.reverse $ T.takeWhile isDigit $ T.drop 1 $ T.reverse s
-      Just pubdate = parseTime defaultTimeLocale "%s" $ T.unpack n
+      Just pubdate = parseTimeM True defaultTimeLocale "%s" $ T.unpack n
       pubdateString = formatTime defaultTimeLocale rfc822DateFormat ( pubdate :: ZonedTime )
   let unpack = T.unpack . T.unwords . T.words
   return $ withItemLink (uri ++ T.unpack d ++ T.unpack href)
